ca2fbc893a6edd4256a4d891965935b65bcb57bf
Leonardo Vinícius de Souza <leonardo.vinicius@italo.br>
2025-05-08 17:52:31 -0300

    navbar completa e inicio da agenda

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    const checkboxes = document.querySelectorAll(".bi-check-square");
-    checkboxes.forEach(box => {
-        box.addEventListener("click", () => {
-            box.parentElement
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".icon").forEach(item => {
+        item.addEventListener("mouseover", () => {
+
+            item.children[0].style.color = "#ed5752";
+            console.log(item.children[1].children[1].style.width);
+        });
+        item.addEventListener("mouseleave", () => {
+            item.children[0].style.color = "#61a3d4";
+            item.children[1].children[1].style.width = "0%";
         });
     });
 });
