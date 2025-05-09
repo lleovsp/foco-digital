f173cbb474351d449f6673816a2b9ec0d8289092
Leonardo <leonardo.frpsouza@gmail.com>
2025-05-08 22:43:11 -0300

    andamento do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".icon").forEach(item => {
-        item.addEventListener("mouseover", () => {
-
-            item.children[0].style.color = "#ed5752";
-            console.log(item.children[1].children[1].style.width);
-        });
-        item.addEventListener("mouseleave", () => {
-            item.children[0].style.color = "#61a3d4";
-            item.children[1].children[1].style.width = "0%";
-        });
-    });
+	document.querySelectorAll(".icon").forEach((item) => {
+		item.addEventListener("mouseover", () => {
+			item.children[0].style.color = "#ed5752";
+			item.children[1].children[1].style.width = "100%";
+		});
+		item.addEventListener("mouseleave", () => {
+			item.children[0].style.color = "#61a3d4";
+			item.children[1].children[1].style.width = "0%";
+		});
+	});
+	const toDoItems = document.querySelectorAll(".to-do-item");
+	toDoItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			console.log("GIRO");
+			item.children[0].lastChild.style.transform = "rotateX(90deg)";
+		});
+	});
 });
